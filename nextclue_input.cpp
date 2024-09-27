3ed270a3977aabf744b78a059c55eab7a147c3bc
Sarah Hashempour <sarahhashempour@Sarahs-MacBook-Pro.local>
2024-09-27 12:57:16 -0400

    Added nextclue_input.cpp with initial content

---
+++ nextclue_input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "This file was useless, so I made it better..." << endl;
+    // You can modify this part as per your game's requirements
+    return 0;
+}
+
